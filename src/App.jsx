704a469b539c92b7b3faf55425c4e082eb1e2079
kg <kylemaxgray@gmail.com>
2025-03-28 10:33:56 -0700

    fix type: missing components path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobsListings from './components/JobsListings'
-import ViewAllJobs from './ViewAllJobs'
+import ViewAllJobs from './components/ViewAllJobs'
+
 const App = () => {
   return (
     <>

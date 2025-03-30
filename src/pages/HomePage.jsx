aa3883fc4effe5c60b5d523dcf74393a5c0959c1
kg <kylemaxgray@gmail.com>
2025-03-29 23:50:20 -0700

    create layouts and pages dirs

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero.jsx';
+import HomeCards from '../components/HomeCards.jsx';
+import JobListings from '../components/JobsListings.jsx';
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+    </>
+  )
+}
+
+export default HomePage

68aface19f16ed7c5f52e8d9822e2bed7c47d951
Kyle <kylemaxgray@gmail.com>
2025-04-01 13:23:02 -0700

    implement single job detail page

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import HomePage from './pages/HomePage.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFound from './pages/NotFound.jsx';
-import JobPage from './pages/JobPage.jsx'
+import JobPage, { jobLoader } from './pages/JobPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />}/>
       <Route path='/jobs' element={<JobsPage />}/>
-      <Route path='jobs/:id' element={<JobPage />}/>
+      <Route path='jobs/:id' element={<JobPage />} loader={jobLoader}/>
       <Route path='*' element={<NotFound/>}/>
     </Route>
   )

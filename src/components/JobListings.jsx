0bbcf45fe7ea4e97c8477ce3fa9c945df8052500
Kyle <kylemaxgray@gmail.com>
2025-04-01 08:27:05 -0700

    implement api route proxy
    add spinner loading component

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from '../components/Spinner.jsx';
 
 const JobListings = ( { isHome = false } ) => {
   const [jobs, setJobs] = useState([]);
@@ -7,6 +8,9 @@ const JobListings = ( { isHome = false } ) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiURL = isHome
+        ? '/api/jobs?_limit=3'
+        : '/api/jobs';
       try {
         const res = await fetch('http://localhost:5000/jobs');
         const data = await res.json();
@@ -26,13 +30,15 @@ const JobListings = ( { isHome = false } ) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           { isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-
-        { jobs.map((job) => (
-          <JobListing key={job.id} job={ job } />
-
+          {loading ? (
+            <Spinner loading={loading}/>
+          ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              { jobs.map((job) => (
+                <JobListing key={job.id} job={ job } />
         ))}
         </div>
+          )}
       </div>
     </section>
   )
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      'api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '');
+      }
+    }
+  }
     react(),
     tailwindcss(),
   ],

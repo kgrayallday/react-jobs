33795850f7461cb60dafa4fc5e73b0cf41807985
Kyle <kylemaxgray@gmail.com>
2025-04-01 19:49:22 -0700

    implemented addJobPage
    - there seems to be an issue with the post request, seems to be going
      out as a get request and the body is causing issues. getting error:
    
    Uncaught (in promise) TypeError: Failed to execute 'fetch' on 'Window': Request with GET/HEAD method cannot have body.

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,33 @@ import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFound from './pages/NotFound.jsx';
 import JobPage, { jobLoader } from './pages/JobPage.jsx'
+import AddJobPage from './pages/AddJobPage.jsx';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />}/>
-      <Route path='/jobs' element={<JobsPage />}/>
-      <Route path='jobs/:id' element={<JobPage />} loader={jobLoader}/>
-      <Route path='*' element={<NotFound/>}/>
-    </Route>
-  )
-)
 const App = () => {
-  return (
-    <>
-    <RouterProvider router={router} />
-    </>
-  )
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      mothod: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />}/>
+        <Route path='/jobs' element={<JobsPage />}/>
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='jobs/:id' element={<JobPage />} loader={jobLoader}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
         target: 'http://localhost:5000/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+      },
+    },
   },
 })
